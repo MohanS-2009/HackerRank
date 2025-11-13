0b4a20747298893496bec54513e0988b03016882
Mohan S <165645174+MohanS-2009@users.noreply.github.com>
2025-11-13 21:33:42 +0530

    Add files via upload

---
+++ SQL/Weather Observation Station 3.sql
@@ -0,0 +1,13 @@
+/* 
+Query a list of CITY names from STATION for cities that have an even ID number.
+Print the results in any order, but exclude duplicates from the answer.
+
+*/
+
+/*
+Enter your query here.
+*/
+
+SELECT DISTINCT CITY 
+FROM STATION 
+WHERE MOD(ID, 2) = 0;
