7e752ed9aaf14f844f4b8e38078327e41200d48f
Mohan S <165645174+MohanS-2009@users.noreply.github.com>
2025-11-29 21:17:45 +0530

    Add files via upload

---
+++ SQL/Japan Population.sql
@@ -0,0 +1,9 @@
+/* 
+
+Query the sum of the populations for all Japanese cities in CITY. The COUNTRYCODE for Japan is JPN.
+
+*/ 
+
+SELECT SUM(POPULATION) AS population
+FROM CITY
+WHERE COUNTRYCODE = 'JPN'
